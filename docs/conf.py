a748368dda36c397f74d7c941ce0eaefd4d17a6e
Gabriel Cardona <bielcardona@gmail.com>
2020-03-06 09:33:25 +0100

    doc conf

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,9 @@ author = 'Biel Cardona'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+master_doc = 'index'
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
