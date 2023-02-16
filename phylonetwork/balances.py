2ab380e6a7c91466aa2641faf4bdbcf60978d656
Biel Cardona <bielcardona@gmail.com>
2023-02-16 22:09:52 +0100

    extended Sackin

--- phylonetwork/balances.py
+++ phylonetwork/balances.py
@@ -1,10 +1,18 @@
 """
 Implementation of different balance indices for phylogenetic trees or networks.
 """
+import phylonetwork
+
 
 def sackin(t):
     """Computes the Sackin (un)balance index of `t`"""
     return sum([t.depths[u] for u in t.leaves])
+
+def extended_sackin(net:phylonetwork.PhylogeneticNetwork):
+    """Computes the extended Sackin (un)balance index of `net`"""
+    factor = sum(net.mu(net.root))
+    weights = [sum(net.mu(v)) for v in net.tree_nodes]
+    return sum(weights)/factor
 
 
 def entropy(t):
